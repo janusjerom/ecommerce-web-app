f5d811534d640845e36f12ef0522f4d6a51fb669
janusjerom <janussmartboy@gmail.com>
2021-11-03 23:20:46 +0530

    removed warnings and unhooked main.js and combined with app.js

--- src/Screens/ProductScreen.js
+++ src/Screens/ProductScreen.js
@@ -5,8 +5,6 @@ import {CardMedia, Card, Button, ButtonGroup, Typography} from '@mui/material';
 import Grid from '@mui/material/Grid';
 import Chip from '@mui/material/Chip';
 import Dialog from '@mui/material/Dialog';
-import DialogTitle from '@mui/material/DialogTitle';
-import DialogContent from '@mui/material/DialogContent';
 import DialogActions from '@mui/material/DialogActions';
 import IconButton from '@mui/material/IconButton';
 import CloseIcon from '@mui/icons-material/Close';
